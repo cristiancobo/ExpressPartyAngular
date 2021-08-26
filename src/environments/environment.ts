62ebcb7a6679caa1a7f559d0bc1442a26a3768a7
Cristian Cobo <cristian.cobo1@u.icesi.edu.co>
2021-08-26 17:20:57 -0500

    cambio url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  endpoint: 'http://localhost:8080/reservas'
+  endpoint: 'https://production-express-party.azurewebsites.net/reservas'
 };
 
 /*
